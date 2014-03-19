a2994a7c5b70075cbb16e28a4697a820f100de00
Rebecca Uebachs <Rebecca@Rebeccas-MacBook-Pro.local>
2014-03-19 15:50:40 +0100

    A 1
    updated

--- server.js
+++ server.js
@@ -1,59 +1,25 @@
 var http = require('http');
 
-
-var planeten = new Array();
-
-planeten[0] = new Object();
-planeten[0]["Name"]= "Merkur";
-planeten[0]["Durchmesser"]= "4879 km";
-planeten[0]["Entfernung"]= "58 Mio km";
-
-planeten[1] = new Object();
-planeten[1]["Name"]= "Venus";
-planeten[1]["Durchmesser"]= "12103 km";
-planeten[1]["Entfernung"]= "108 Mio km";
-
-planeten[2] = new Object();
-planeten[2]["Name"]= "Erde";
-planeten[2]["Durchmesser"]= "12734 km";
-planeten[2]["Entfernung"]= "150 Mio km";
-
-planeten[3] = new Object();
-planeten[3]["Name"]= "Mars";
-planeten[3]["Durchmesser"]= "6772 km";
-planeten[3]["Entfernung"]= "228 Mio km";
-
-planeten[4] = new Object();
-planeten[4]["Name"]= "Jupiter";
-planeten[4]["Durchmesser"]= "138346 km";
-planeten[4]["Entfernung"]= "778 Mio km";
-
-planeten[5] = new Object();
-planeten[5]["Name"]= "Saturn";
-planeten[5]["Durchmesser"]= "114632 km";
-planeten[5]["Entfernung"]= "1433 Mio km";
-
-planeten[6] = new Object();
-planeten[6]["Name"]= "Uranus";
-planeten[6]["Durchmesser"]= "50532 km";
-planeten[6]["Entfernung"]= "2872 Mio km";
-
-planeten[7] = new Object();
-planeten[7]["Name"]= "Neptun";
-planeten[7]["Durchmesser"]= "49105 km";
-planeten[7]["Entfernung"]= "4495 Mio km";
-
+planeten = [
+{ "Name":"Merkur" , "Durchmesser":"4879 km" , "Entfernung":"58 Mio km" }, 
+{ "Name":"Venus" , "Durchmesser":"12103 km" , "Entfernung":"108 Mio km"}, 
+{ "Name":"Erde" , "Durchmesser":"12734 km" , "Entfernung":"150 Mio km"},
+{ "Name":"Mars" , "Durchmesser":"6772 km" , "Entfernung":"228 Mio km" }, 
+{ "Name":"Jupiter" , "Durchmesser":"114632 km" , "Entfernung":"778 Mio km"}, 
+{ "Name":"Saturn" , "Durchmesser":"12734 km" , "Entfernung":"1433 Mio km"},
+{ "Name":"Uranus" , "Durchmesser":"50532 km" , "Entfernung":"2872 Mio km" }, 
+{ "Name":"Neptun" , "Durchmesser":"49105 km" , "Entfernung":"4495 Mio km"}
+]
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-    
-for (var i = 0; i < 7; i++) {
-  res.write('<dl><dt>Planet ' + (i + 1) + '</dt>');
-  for (var Eigenschaft in planeten[i])
-    res.write('<dd>' + Eigenschaft + ': ' + planeten[i][Eigenschaft] + '</dd>');
-  res.write('</dl>');
+    res.write("<table border=1>");
+    res.write("<tr><th>Name</th><th>Durchmesser</th><th>Entfernung</th>");
+for (var i = 0; i < 8; i++) {
+  res.write("<tr><td>" + planeten[i].Name + "</td><td>" + planeten[i].Durchmesser + "</td><td>" + planeten[i].Entfernung + "</td></tr>");
 }
-    
+ 
+ res.write("</table>");
  res.end();
                   }).listen(8124, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:8124/');
