3f2e286bd5d976cae40718438fa64b063569bf4a
Rebecca Uebachs <Rebecca@Rebeccas-MacBook-Pro.local>
2014-04-02 16:04:58 +0200

    neu

--- server.js
+++ server.js
@@ -1,43 +1,143 @@
-var http = require('http');
-var url = require('url');
+var express = require('express');
+var mongoDB = require('mongoskin');
 
+var db = mongoDB.db('mongodb://localhost/mydb?auto_reconnect=true',{
+    safe: true});
+
+db.bind("planeten");
+
+var planetenCollection = db.planeten;
+
+planetenCollection.insert([
+{
+    Name: "Merkur",
+    Durchmesser: "4879",
+    Entfernung: "58"
+},{
+    Name: "Venus",
+    Durchmesser: "12103",
+    Entfernung: "108"
+},{
+    Name: "Erde",
+    Durchmesser: "12734",
+    Entfernung: "150"
+},{
+    Name: "Mars",
+    Durchmesser: "6772",
+    Entfernung: "228"
+},{
+    Name: "Jupiter",
+    Durchmesser: "114632",
+    Entfernung: "778"
+},{
+    Name: "Saturn",
+    Durchmesser: "12734",
+    Entfernung: "1433"
+},{
+    Name: "Uranus",
+    Durchmesser: "50532",
+    Entfernung: "2872"
+},{
+    Name: "Neptun",
+    Durchmesser: "49105",
+    Entfernung: "4495"                       
+},function (err, planeten){
+     if (err){
+            next(err);
+        }else{
+            res.writeHead(200,'OK');
+            res.write('Daten wurden gespeichert');
+            res.end(JSON.stringify(planten));
+        }
+}]);
+                
+
+/*
+* Json Array mit Objekten
+*/
 planeten = [
-{ "Name":"Merkur" , "Durchmesser":"4879 km" , "Entfernung":"58 Mio km" }, 
-{ "Name":"Venus" , "Durchmesser":"12103 km" , "Entfernung":"108 Mio km"}, 
-{ "Name":"Erde" , "Durchmesser":"12734 km" , "Entfernung":"150 Mio km"},
-{ "Name":"Mars" , "Durchmesser":"6772 km" , "Entfernung":"228 Mio km" }, 
-{ "Name":"Jupiter" , "Durchmesser":"114632 km" , "Entfernung":"778 Mio km"}, 
-{ "Name":"Saturn" , "Durchmesser":"12734 km" , "Entfernung":"1433 Mio km"},
-{ "Name":"Uranus" , "Durchmesser":"50532 km" , "Entfernung":"2872 Mio km" }, 
-{ "Name":"Neptun" , "Durchmesser":"49105 km" , "Entfernung":"4495 Mio km"}
+{ "Name":"Merkur" , "Durchmesser":"4879" , "Entfernung":"58" }, 
+{ "Name":"Venus" , "Durchmesser":"12103" , "Entfernung":"108"}, 
+{ "Name":"Erde" , "Durchmesser":"12734" , "Entfernung":"150"},
+{ "Name":"Mars" , "Durchmesser":"6772" , "Entfernung":"228" }, 
+{ "Name":"Jupiter" , "Durchmesser":"114632" , "Entfernung":"778"}, 
+{ "Name":"Saturn" , "Durchmesser":"12734" , "Entfernung":"1433"},
+{ "Name":"Uranus" , "Durchmesser":"50532" , "Entfernung":"2872" }, 
+{ "Name":"Neptun" , "Durchmesser":"49105" , "Entfernung":"4495"}
 ]
 
-http.createServer(function (req, res) {
+/*
+* Webserver starten
+* Verzeichnis fuer den direkten Zugriff von Aussen freigeben
+* Request Information parsen
+*/
 
-    var pfad = url.parse(req.url).pathname;
+
+
+
+var app = express();
+app.configure(function(){
+	app.use(express.static(__dirname + '/public'));
+	app.use(express.json());
+});
+
+
+/*
+* Bei GET auf /planeten Tabelle ausgeben
+*/
+app.get('/planeten', function(req, res, next){
+	res.writeHead(200, {'Content-Type': 'text/html'});
     
-    if(pfad!='/planeten'){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('Errror Falscher Pfad <br>Klicken Sie: <a href="/planeten">Hier</a>');
-        res.end();
+    /*
+    Daten aus der DB abrufen
+    */
+    planetenCollection.findItemd(function (err, result){
+        if (err)
+            next(err);
+        
+    else{
+        res.writeHead(200,{
+            'Content-Type': 'application/json'});
+        res.end(JSON.stringify(planten));}
+    });
+});  
+    
+/*	res.write('<table border="1">');
+	res.write('<tr><th>Name</th><th>Durchmesser</th><th>Entfernung</th>');
+		 for (var i = 0; i < planeten.length; i++) {
+			res.write("<tr><td>" + planeten[i].Name + "</td><td>" + planeten[i].Durchmesser + " km </td><td>" + planeten[i].Entfernung + " Mio km</td></tr>");
+		}
+	res.write('</table>');
+	res.end();
+});
+/*
+/*
+* Ankommenden POST auf /planeten zu planeten Array hinzufuegen und auf Konsole ausgeben
+*/
+app.post('/planeten', function(req, res, next){
+	
+    planetenCollection.insert(req.body, function (err, result){
+        if (err){
+            next(err);
+        }else{
+            res.writeHead(200,'OK');
+            res.write('Daten wurden gespeichert');
+            res.end();
         }
+    });
+});
+    
+    /*
+    var empfangen = JSON.stringify(req.body);
+	planeten.push(req.body);
+	console.log("Objekt hinzugefuegt: " + empfangen);
+	res.end();
+    /*
+});
 
-    
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("<table border=1>");
-    res.write("<tr><th>Name</th><th>Durchmesser</th><th>Entfernung</th>");
-for (var i = 0; i < 8; i++) {
-  res.write("<tr><td>" + planeten[i].Name + "</td><td>" + planeten[i].Durchmesser + "</td><td>" + planeten[i].Entfernung + "</td></tr>");
-}
- res.write("</table>");   
-    
-    res.write('<form action="/planeten" method="POST">');   
-    res.write('Planet: <input name="name" type="text"><br>');  
-    res.write('Durchmesser: <input name="durchmesser" type="text"><br>');
-    res.write('Entfernung: <input name="entfernung" type="text"><br>');
-    res.write('<input name="submit" type="submit">');
-    res.write('</form>');   
-    
-        res.end();
-}).listen(8124, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8124/');
+/*
+* Server an Port 3000 binden
+*/
+var server = app.listen(3000, function() {
+    console.log('Listening on port %d', server.address().port);
+});
