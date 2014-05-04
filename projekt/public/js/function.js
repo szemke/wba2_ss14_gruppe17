4cefb6397249773425b32e650d081b5b3d9aee5b
Simon Zemke <simon.zemke@gmail.com>
2014-05-04 18:54:08 +0200

    Lieferdienst eintragen, anzeigen
    basic

--- projekt/public/js/function.js
+++ projekt/public/js/function.js
@@ -36,6 +36,67 @@ function adduser(){
 	}
 }
 /*
+ * REGISTRATION
+ */
+function addservice(){
+	/*
+	* Pruefe das Formularfelder nicht leer sind
+	*/
+	if($('#restaurant').val() != "" && $('#phone').val() != "" && $('#ruhe').val() != "" && $('#ort').val() != "" && $('#strasse').val() != "" && $('#plz').val() != "" && $('#nr').val() != "" && $('#min').val() != "" && $('#anfahrt').val() != "" && $('#date').val() != ""){
+			
+	/*
+	* Email Adresse als Salt dem Passwort hinzufügen und mit SHA512 Hashwert erstellen.
+	* Anschlißend zu Data Objekt hinzufügen
+	*/
+	var data = {restaurant: $('#restaurant').val(), phone: $('#phone').val(),ruhe: $('#ruhe').val(), ort: $('#ort').val(), strasse: $('#strasse').val(), plz: $('#plz').val(), nr: $('#nr').val(), min: $('#min').val(), anfahrt: $('#anfahrt').val(), date: $('#date').val()};
+			
+	/*
+	* AJAX Post absenden vom Typ Jason Objekt
+	* Bei Erfolg: Alert ausgeben, Formularfelder leeren und auf Index.html weiterleiten
+	* Bei Misserfolg: alert ausgeben
+	*/
+	$.ajax({
+		type: 'POST',
+		url: '/addservice',
+		data: JSON.stringify(data),
+		contentType: 'application/json'
+		}).done(function(){
+			alert(data.name+' wurde hinzugefuegt.');
+			window.location = "/";
+		}).fail(function(e){
+			alert(data.name+' konnte nicht hinzugefuegt werden. ('+JSON.stringify(e)+')');
+		});
+	}else{
+		alert(unescape("Bitte alle Felder ausf%FCllen")); //unescape: Umlaut uebersetzen
+	}
+}
+/*
+ *
+ */
+function getservices(){
+var request = $.ajax({
+            type: 'GET',
+            url: 'http://localhost:3000/getservice',
+            contentType: 'application/json'
+        });
+        
+        request.done(function(services){
+            services.forEach(function(service){
+                addTableRow(service);
+            });
+        });
+        
+        request.fail(function(err){
+			alert("something is wrong");
+        });
+		
+	function addTableRow(service){
+        $('#lieferservice').append('<tr><td><img src="gfx/pizza.png" width="100px" height="100px"></td><td id="service">'+service.restaurant+'<br />'+service.strasse+' '+service.nr+'<br />'+service.plz+' '+service.ort+'<br />Tel.:'+service.phone+'<p>Mindestbestellwert: '+service.min+' <br />Anfahrtskosten: '+service.anfahrt+'</p></td>'+
+			'<td id="date">Ruhetag: '+service.ruhe+'<br />'+service.date+'</td></tr>');
+    }
+}
+
+/*
  * Authentifizierung
  */
 function auth(){
